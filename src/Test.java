002bdfc90b109fe7b696c7c182b7c401de82c417
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2021-11-24 10:13:12 +0100

    Saker

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,26 @@
-import javax.swing.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Test {
-    public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Välj 7 nnummer ");
-        int x = Integer.parseInt(a);
-        int k = x/7;
-        String medel = "Medelvärdet är" + k;
-        JOptionPane.showMessageDialog(null,  medel);
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader
+                (new InputStreamReader(System.in));
+        String kons = "bcdfghjklmnqprstvxzBCDFGHJKLMNQPRSTVXZ";
+        while (true) {
+            String s1 = input.readLine();
+            if (s1 == null)
+                break;
+            String s2 = "";
 
+            for (int i=0;i< s1.length(); i++ ) {
+                char c = s1.charAt(i);
+                s2 = s2 + c;
+                if (kons.indexOf (c)>=0)
+                    s2 = s2 + "o" + c;
+            }
+            System.out.println(s2);
+        }
     }
 }
